af47e6765f894e5373c691b6f3e9aea19e9351e9
mwendell <mwendellsmce@gmail.com>
2026-04-27 15:11:51 -0300

    config deploy

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "https://farmacia-nest.onrender.com"
+  baseURL: import.meta.env.VITE_API_URL
 })
 
 export const buscar = async (url: string, setDados: Function) => {
